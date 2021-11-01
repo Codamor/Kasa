876492f4f8db6f10e2ad9cd67b50d761cbbd1f12
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-11-01 14:03:49 +0100

    add Logo component

---
+++ src/Components/Header/Logo/Logo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Logo.css" ;
+
+class Logo extends React.Component{
+    render(){
+        return (
+            <h1 className="logo">
+                Kasa
+            </h1>
+        )
+    }
+}
+
+export default Logo ;
