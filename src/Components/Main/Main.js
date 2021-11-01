8e21b62fc6d8778d8993d2a18410875381660244
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-11-01 15:01:15 +0100

    add Main component

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Main.css" ;
+
+class Main extends React.Component{
+    render(){
+        return (
+            <main>
+
+            </main>
+        )
+    }
+}
+
+export default Main ;
