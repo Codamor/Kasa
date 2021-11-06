f0ecb257392c4bb87274971267b55cc307d2bfd9
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-11-06 10:40:12 +0100

    remove Banner props

--- src/Components/Main/Banner/Banner.js
+++ src/Components/Main/Banner/Banner.js
@@ -2,9 +2,6 @@ import React from "react";
 import "./Banner.css" ;
 
 class Banner extends React.Component{
-    constructor(props) {
-        super(props);
-    }
     render() {
         return (
             <div className="banner">
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Main.css" ;
+import Banner from "./Banner/Banner";
 
 class Main extends React.Component{
     render(){
         return (
             <main>
-
+                <Banner />
             </main>
         )
     }
