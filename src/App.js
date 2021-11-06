985ca3b4b6f02461ab1fa34eb2b1964934dbcb8b
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-11-06 10:40:32 +0100

    add Main component to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {BrowserRouter} from "react-router-dom" ;
 import Header from "./Components/Header/Header";
+import Main from "./Components/Main/Main";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      <Main />
     </BrowserRouter>
   );
 }
