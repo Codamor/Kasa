393fdbfbd3431c10fa5a058715a5d6f175bdaaa4
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-11-13 18:10:35 +0100

    add onClick function on title

--- src/Components/Main/Toggle/Toggle.js
+++ src/Components/Main/Toggle/Toggle.js
@@ -4,18 +4,43 @@ import "./Toggle.css" ;
 class Toggle extends React.Component{
     constructor(props) {
         super(props);
+
+        this.state = {
+            visible: "false"
+        }
+
+        this.toggle = this.toggle.bind(this) ;
+
+    }
+
+    toggle(){
+        if (this.state.visible === "true"){
+            this.setState(
+                {
+                    visible: "false"
+                }
+            )
+        } else {
+            this.setState(
+                {
+                    visible: "true"
+                }
+            )
+        }
     }
 
     render(){
         return(
             <div className="toggle">
-                <div className="toggle__title">
+                <div className="toggle__title" onClick={this.toggle}>
                     <h3>{this.props.title}</h3>
                 </div>
-                <div className="toggle__content">
+                <div className="toggle__content" visible={this.state.visible}>
                     <p>{this.props.text}</p>
                 </div>
             </div>
+
+
         )
     }
 }
