a63f17cad823bce32a1f0d3920c0dfe3c353f7b9
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-11-15 17:01:20 +0100

    add props for dynamic content

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -2,13 +2,20 @@ import React from "react";
 import "./Banner.css" ;
 
 class Banner extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <div className="banner">
                 <div className="banner__overlay">
                 </div>
+
+                <img src={this.props.picture} alt={this.props.alt} title={this.props.title}/>
+
                 <h2 className="banner__title">
-                    Chez vous, partout et ailleurs
+                    {this.props.text}
                 </h2>
             </div>
         );
