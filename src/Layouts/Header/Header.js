ed9a7f5c378e7046df34f4b1dfcf7fda1e9f6407
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-11-16 14:37:05 +0100

    add dynamic logo and Header logo update

--- src/Layouts/Header/Header.js
+++ src/Layouts/Header/Header.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./Header.css" ;
 import Logo from "../../Components/Logo/Logo" ;
 import Navigation from "../../Components/Navigation/Navigation";
+import logoImage from "../../assets/img/logo.png"
 
 class Header extends React.Component{
     render() {
         return (
              <header className="header">
-                 <Logo />
+                 <Logo logo={logoImage} />
                  <Navigation />
              </header>
         );
