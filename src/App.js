d86f0b5d33fde8931dbb4830765d95ca53328698
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-11-18 13:33:16 +0100

    add routes for home and about

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
-import {BrowserRouter} from "react-router-dom" ;
+import {BrowserRouter, Switch, Route} from "react-router-dom" ;
+
 import Header from "./Layouts/Header/Header";
+import Footer from "./Layouts/Footer/Footer";
 import MainAbout from "./Layouts/Main/MainAbout";
+import MainHome from "./Layouts/Main/MainHome";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <MainAbout />
-    </BrowserRouter>
+      <BrowserRouter>
+          <Header />
+            <Switch>
+                <Route exact path="/" component={MainHome} />
+                <Route path={"/about"} component={MainAbout} />
+            </Switch>
+          <Footer />
+      </BrowserRouter>
+
   );
 }
 
