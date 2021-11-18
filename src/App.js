5f056550caa390530dd28cfb05a5c5220fd1e038
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-11-18 13:36:11 +0100

    add route for 404 url

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./Layouts/Header/Header";
 import Footer from "./Layouts/Footer/Footer";
 import MainAbout from "./Layouts/Main/MainAbout";
 import MainHome from "./Layouts/Main/MainHome";
+import Main404 from "./Layouts/Main/Main404";
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
           <Header />
             <Switch>
                 <Route exact path="/" component={MainHome} />
-                <Route path={"/about"} component={MainAbout} />
+                <Route path="/about" component={MainAbout} />
+                <Route path="*" component={Main404} />
             </Switch>
           <Footer />
       </BrowserRouter>
